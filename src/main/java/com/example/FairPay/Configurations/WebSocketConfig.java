57fd53fe4a980f43526521ee7fb8a533f658be43
chetanakashV <sherlockdetective69@gmail.com>
2024-06-25 17:47:00 +0530

    complete project

--- src/main/java/com/example/FairPay/Configurations/WebSocketConfig.java
+++ src/main/java/com/example/FairPay/Configurations/WebSocketConfig.java
@@ -1,6 +1,9 @@
 package com.example.FairPay.Configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -18,7 +21,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS().setWebSocketEnabled(true);
     }
 
 
